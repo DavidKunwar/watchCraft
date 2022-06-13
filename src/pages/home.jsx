19ff75dd83cd15e180465867d1c780a37271f123
David Kunwar <dkunwar7111@gmail.com>
2022-06-13 14:43:16 +0530

    spell check at home page, discover route is dynamic now

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -9,7 +9,7 @@ function Home(){
         <div className="home">
             <img className='bg-img' src='https://assets.nflxext.com/ffe/siteui/vlv3/25badb14-858b-4b1c-8b7d-2244098454d9/c18df626-e6fc-4dbb-818a-55d299b11842/IN-en-20220606-popsignuptwoweeks-perspective_alpha_website_large.jpg' />
             <div className="home-text">
-                Watch Usnlimited Movie Trailers
+                Watch Unlimited Movie Trailers
                 <br/>
                 <Button href='/discover' className="get-started-btn" variant="danger" size="lg">Get Started <ArrowForwardIosIcon/></Button>
             </div>
